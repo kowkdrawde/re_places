9aa140957397e9c7f5721ecb32e2041dba472f14
Edward <kowkdrawde@gmail.com>
2015-12-14 22:05:37 +0800

    Added maps to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
   validates :name, :phone, :address, :website, :user_id, presence: true
+
+  geocoded_by :address
+  after_validation :geocode
 end
